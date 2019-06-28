b41623eda7ac466b48b7c11254e6655685a3ddee
1STG\osawa.y <osawa.yoshiki@gmail.com>
2019-06-29 02:17:07 +0900

    send test2mbc

--- liff-app/linethings-dev-default/liff.js
+++ liff-app/linethings-dev-default/liff.js
@@ -321,6 +321,7 @@ function onConnectMQTT() {
     message = new Paho.MQTT.Message(jsonbody);
     message.destinationName = "test/osawa";
     client.send(message);
+    client.disconnect();
 }
 function doFailMQTT(e){
     onScreenLog(e);
@@ -371,7 +372,7 @@ function send2MB(device, buffer){
     const sw2 = buffer.getInt16(10, true);
     let datetime = Date.now();
 
-    jsonbody = '{"protocol": "1.0","loginId": "osawa.y","template": "iot","tenant": "cdl002mb","status":[{"time":' + datetime + ',"enabled": "true","values":[{"name":"temp","type":"3","value":' + temperature + '}]}]}';
+    jsonbody = '{"protocol": "1.0","loginId": "osawa.y","id":"l01","template": "iot","tenant": "cdl002mb","status":[{"time":' + datetime + ',"enabled": "true","values":[{"name":"temp","type":"3","value":' + temperature + '}]}]}';
 
     ws_mqtt();
 
